52ace5984f2961162c1e80885b6f6ba0ad3e85f6
nataly tuardy <nattaurdy@gmail.com>
2021-10-08 16:19:52 +0300

    deleted logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-// import logo from './logo.svg';
 import './App.css';
 import { Switch, Route, Redirect, Link } from "react-router-dom";
-import Test from './components/Test';
 import testPage from './pages/testPage';
 import typePage from './pages/typePage';
 import Result from './components/ResultComponent';
@@ -9,11 +7,6 @@ import Result from './components/ResultComponent';
 function App() {
   return (
     <div className="App">
-     {/* <h1 className="title">Тест на определение личности</h1>
-     <h3 className="answer">Выберите слово, которое, на Ваш взгляд, наиболее точно характеризует Вас.</h3>
-     <div  className="buttonsWrap">
-    <Test/>
-    </div> */}
     <Switch>
               <Route path="/page" exact component={testPage} />
               <Route path="/type" exact component={typePage} />
